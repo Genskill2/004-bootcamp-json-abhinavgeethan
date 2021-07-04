98f3adbfc5a33e418e9319214ddde361004cf678
abhinavgeethan <abhinavgeethan@gmail.com>
2021-07-04 08:53:41 +0400

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -6,43 +6,33 @@ def load_journal(file_name):
         return json.load(f)
 
 def compute_phi(file_name,event):
-    a=1
-    b=1
-    c=1
-    d=1
-    e=1
-    f=1
-    g=1
-    h=1
-    ctr=0
+    a=0
+    b=0
+    c=0
+    d=0
+    e=0
+    f=0
+    g=0
+    h=0
     with open(file_name,'r') as file:
         data=json.load(file)
         for datum in data:
             if (event in datum['events']) and (datum['squirrel']=='False'):
-                if not ctr==0:
-                    a+=1
-            if (event not in datum['events']) and (datum['squirrel']!='False'):
-                if not ctr==0:
-                    b+=1
+                a+=1
+            if (not(event in datum['events'])) and (datum['squirrel']!='False'):
+                b+=1
             if (event in datum['events']) and (datum['squirrel']!='False'):
-                if not ctr==0:
-                    c+=1
-            if (event not in datum['events']) and (datum['squirrel']=='False'):
-                if not ctr==0:
-                    d+=1
+                c+=1
+            if (not(event in datum['events'])) and (datum['squirrel']=='False'):
+                d+=1
             if (event in datum['events']):
-                if not ctr==0:
-                    e+=1
-            if (event not in datum['events']):
-                if not ctr==0:
-                    f+=1
+                e+=1
+            if not (event in datum['events']):
+                f+=1
             if (datum['squirrel']=='False'):
-                if not ctr==0:
-                    g+=1
+                g+=1
             if (datum['squirrel']!='False'):
-                if not ctr==0:
-                    h+=1
-            ctr+=1
+                h+=1
     return (a*b-c*d)/math.sqrt(e*f*g*h)
 
 def compute_correlations(file_name):
